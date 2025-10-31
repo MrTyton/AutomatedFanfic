0887ce3e9329f3c695655270dab85298de8ef67f
MrTyton <MrTyton@users.noreply.github.com>
2025-10-31 07:21:16 -0400

    bump: minor version to 1.19.0 (Move more things to debug logging)

--- root/app/fanficdownload.py
+++ root/app/fanficdownload.py
@@ -46,7 +46,7 @@ from config_models import ConfigManager, ConfigError, ConfigValidationError
 from process_manager import ProcessManager
 
 # Define the application version
-__version__ = "1.18.0"
+__version__ = "1.19.0"
 
 
 def parse_arguments() -> argparse.Namespace:
