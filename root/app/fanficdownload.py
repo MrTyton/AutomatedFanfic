60e61776eddf10b8d17842def389f8d31c7f9e01
MrTyton <MrTyton@users.noreply.github.com>
2026-01-02 12:03:47 -0500

    bump: minor version to 2.1.0 (Change color palette for higher visibility logs.)

--- root/app/fanficdownload.py
+++ root/app/fanficdownload.py
@@ -49,7 +49,7 @@ from models.config_models import ConfigError, ConfigValidationError
 from process_management import ProcessManager
 
 # Define the application version
-__version__ = "2.0.2"
+__version__ = "2.1.0"
 
 
 def parse_arguments() -> argparse.Namespace:
